3c14703768c7b78bd5e832322f715fd57a2aade3
Hetain <hetain@Hetains-Mac-mini.local>
2025-03-29 17:36:00 +0530

    new changes

--- App.jsx
+++ App.jsx
@@ -4,12 +4,17 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomePage from './Screens/HomePage';
 import Otp from './Screens/Otp/Otp';
-
+import OnboardingScreen from './Screens/HomePageComponents/PreScreens/OnBoardingScreen';
+import Login from './Screens/HomePageComponents/PreScreens/Login';
+import Entry from './Screens/HomePageComponents/PreScreens/Entry';
 const App = () => {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Entry" component={Entry} />
+        <Stack.Screen name="OnBoarding" component={OnboardingScreen} />
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Otp" component={Otp} />
         <Stack.Screen name="Homepage" component={HomePage} />
       </Stack.Navigator>
